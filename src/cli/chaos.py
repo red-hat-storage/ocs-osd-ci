b8ce87c640688d830a8ea229f8de3c1eca027242
Alfonso Martínez <almartin@redhat.com>
2022-05-03 12:27:09 +0200

    provider: add inbound rules via AWSService
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/cli/chaos.py
+++ src/cli/chaos.py
@@ -3,6 +3,7 @@
 import logging
 import sys
 
+from src.service.aws import AWSService
 from src.service.cluster import ClusterService
 from src.util.util import env
 
@@ -13,10 +14,20 @@ def main() -> int:
     logger.info("CHAOS testing: setting up...")
     cluster_service = ClusterService()
 
-    provider_cluster_id = cluster_service.install(env("PROVIDER_CLUSTER_NAME"))
+    # Create provider cluster.
+    provider_cluster_name = env("PROVIDER_CLUSTER_NAME")
+    provider_cluster_id = cluster_service.install(provider_cluster_name)
     logger.info("PROVIDER CLUSTER ID: %s", provider_cluster_id)
 
+    # Create consumer cluster.
+    consumer_cluster_name = env("CONSUMER_CLUSTER_NAME")
+    consumer_cluster_id = cluster_service.install(consumer_cluster_name)
+    logger.info("CONSUMER CLUSTER ID: %s", consumer_cluster_id)
+
+    # Add inbound rules.
     cluster_service.wait_for_cluster_ready(provider_cluster_id)
+    aws_service = AWSService()
+    aws_service.add_provider_addon_inbound_rules(provider_cluster_name)
 
     logger.info("CHAOS testing completed.")
     return 0
---
+++ src/service/aws.py
@@ -0,0 +1,89 @@
+import logging
+
+import boto3
+from mypy_boto3_ec2.client import EC2Client
+from mypy_boto3_ec2.type_defs import (
+    AuthorizeSecurityGroupIngressResultTypeDef,
+    DescribeSecurityGroupsResultTypeDef,
+)
+
+from src.util.util import env
+
+logger = logging.getLogger()
+
+
+class AWSService:
+    _ec2_client: EC2Client
+
+    def __init__(self):
+        self._ec2_client = boto3.client(
+            "ec2",
+            aws_access_key_id=env("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=env("AWS_SECRET_ACCESS_KEY"),
+        )
+
+    def add_provider_addon_inbound_rules(self, cluster_name: str):
+        describe_result: DescribeSecurityGroupsResultTypeDef = (
+            self._ec2_client.describe_security_groups(
+                Filters=[
+                    {
+                        "Name": "tag:Name",
+                        "Values": [
+                            f"{cluster_name}-*-worker-sg",
+                        ],
+                    },
+                ]
+            )
+        )
+        sg_group_id = describe_result["SecurityGroups"][0]["GroupId"]
+
+        authorize_result: AuthorizeSecurityGroupIngressResultTypeDef = (
+            self._ec2_client.authorize_security_group_ingress(
+                GroupId=sg_group_id,
+                IpPermissions=[
+                    {
+                        "FromPort": 6800,
+                        "ToPort": 7300,
+                        "IpProtocol": "tcp",
+                        "IpRanges": [
+                            {"CidrIp": "10.0.0.0/16", "Description": "Ceph OSDs"},
+                        ],
+                    },
+                    {
+                        "FromPort": 3300,
+                        "ToPort": 3300,
+                        "IpProtocol": "tcp",
+                        "IpRanges": [
+                            {"CidrIp": "10.0.0.0/16", "Description": "Ceph MONs rule1"}
+                        ],
+                    },
+                    {
+                        "FromPort": 6789,
+                        "ToPort": 6789,
+                        "IpProtocol": "tcp",
+                        "IpRanges": [
+                            {"CidrIp": "10.0.0.0/16", "Description": "Ceph MONs rule2"},
+                        ],
+                    },
+                    {
+                        "FromPort": 9283,
+                        "ToPort": 9283,
+                        "IpProtocol": "tcp",
+                        "IpRanges": [
+                            {"CidrIp": "10.0.0.0/16", "Description": "Ceph Manager"},
+                        ],
+                    },
+                    {
+                        "FromPort": 31659,
+                        "ToPort": 31659,
+                        "IpProtocol": "tcp",
+                        "IpRanges": [
+                            {"CidrIp": "10.0.0.0/16", "Description": "API Server"},
+                        ],
+                    },
+                ],
+            )
+        )
+        if not authorize_result["Return"]:
+            logger.error(authorize_result)
+            raise RuntimeError("EC2: error while adding inbound rules.")

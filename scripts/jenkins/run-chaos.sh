f37f606d276c9615775d62f40d1ea0f0385755bb
Alfonso Martínez <almartin@redhat.com>
2022-06-01 17:49:34 +0200

    Set up chaos test run
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- scripts/jenkins/run-chaos.sh
+++ scripts/jenkins/run-chaos.sh
@@ -18,14 +18,22 @@ fi
 make install PY_BIN=python3.9
 make install-consumer-addon
 
-# Install ocs-monkey
-if [[ -n "${JENKINS_HOME}" || ! -d ocs-monkey ]]; then
+# Install ocs-monkey.
+if [[ ! -d ocs-monkey ]]; then
     git clone --depth=1 --no-tags https://github.com/red-hat-storage/ocs-monkey
 fi
 cd ocs-monkey
 python3.6 -m venv venv  # @TODO: modify setup-env.sh to allow choosing python version and venv dir name.
 source setup-env.sh
 
-# @TODO: run ocs-monkey.
+# Start workload runner.
+export CONSUMER_KUBECONFIG=../.cluster/consumer-kubeconfig.yaml
+KUBECONFIG="${CONSUMER_KUBECONFIG}" helm install workload ./helm/ocs-monkey-generator \
+     --set workload.runtime=9000
+
+# Start chaos runner.
+export PROVIDER_KUBECONFIG=../.cluster/provider-kubeconfig.yaml
+KUBECONFIG="${PROVIDER_KUBECONFIG}" ./chaos_runner.py -t 7200 --monitor-deployment default/workload-ocs-monkey-generator \
+    --monitor-deployment-cluster-config "${CONSUMER_KUBECONFIG}"
 
 echo "CHAOS testing completed."
